0940604785cbc88809fe9de62d62847b37d12e2c
Amir xajeh <axajeh@gmail.com>
2022-07-29 12:05:14 +0430

    [CREATE]: user insert

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostService } from './post/post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService, PostService],
+  providers: [AppService, PrismaService, PostService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Get("user/:id")
-  async getUserById(@Param("id") id: number): Promise<User> {
-    return this.userService.findOne({
+  @Get(":id")
+  async getUserById(@Param("id", ParseIntPipe) id: number): Promise<User> {
+    return await this.userService.findOne({
       id
     })
+  }
+
+  @Post()
+  async createUser(@Body() userCreateInput: Prisma.UserCreateInput): Promise<User> {
+    console.log(userCreateInput)
+    return await this.userService.create(userCreateInput)
   }
 }
